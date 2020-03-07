368dae1b2cd217acde5da79cf4f21bf7f474f623
Stew Esho <stewardesho@gmail.com>
2020-03-07 17:45:40 -0500

    Added basic AdminDashboard page, edited some routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
 import './App.css';
-
 import Home from './react-components/Home';
+import AdminDashboard from './react-components/AdminDashboard'
+
 class App extends React.Component {
+
+  loggedIn = false;
+
   render() {
       return (
         <div> 
           <BrowserRouter>
             <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */ }
               { /* Each Route below shows a different component depending on the exact path in the URL  */ }
+              {/* TODO: Allow user to login as either admin or user. */}
+              {/* TODO: redirect to seperate login page if user is not logged in. */}
               <Route exact path='/' render={() => (<Home />)}/>
-          </Switch>
+              <Route exact path='/dashboard/admin' render={() => (<AdminDashboard />)}/> { /* TODO: create a dashboard component that will then have its own router for admin/user dashboards */ }
+            </Switch>
+            {/* {this.loggedIn ? <Redirect to="/dashboard/admin" /> : <Redirect to="/login" /> } */}
           </BrowserRouter>
         </div>
       )
---
+++ frontend/src/react-components/AdminDashboard/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+
+/* Primary Component for the Admin Dashboard page */
+class AdminDashboard extends React.Component {
+  // TODO: add router for dashboard subpages
+  render () {
+    return (
+      <div>
+        <p>admin dashboard</p>
+      </div>
+    );
+  }
+}
+
+
+export default AdminDashboard;
