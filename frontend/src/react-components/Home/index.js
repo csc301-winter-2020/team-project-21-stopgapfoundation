a7413c9f1a5d587b9b2f7d09e0303c8ca105c53c
Daven <daven.boparai@mail.utoronto.ca>
2020-03-08 14:19:07 -0400

    Updated Home Page.

--- frontend/src/react-components/Home/index.js
+++ frontend/src/react-components/Home/index.js
@@ -1,57 +1,29 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Button from "@material-ui/core/Button";
-import ReactDOM from 'react-dom';
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button"
+import logo from "./static/logo.png"
+import Typography from '@material-ui/core/Typography';
+
+
 import "./styles.css";
-import Header from "../Header";
-import User from "../Login/user";
-import Admin from "../Login/admin";
 
-
-/* Component for the Home page */
 class Home extends React.Component {
-  render () {
-    return (
-      <Router>
-        <div className="home__bg-image center">
-          <Header />
-          <div className="buttons center">
-            <Link className="home__button-link" to={"./../user"}>
-              <Button className="home__button" >User</Button>
-            </Link>
-            <Link className="home__button-link" to={"./../admin"}>
-              <Button className="home__button">Admin</Button>
-            </Link>
-          </div>
+    render() {
+        return (
+            <div className = "home__bg center">
+                <div id="logoContainer" >
+                    <img id="logo" src={logo}></img>
+                </div>
+                <Link className="home__button-link center_left" to={"./../Login"}>
+                    <Button className="login__button" style={{backgroundColor:'#689BF0'}}>Login</Button>
+                </Link>
+                <Link className="home__button-link center_right" to={"./../Register"}>
+                    <Button className="login__button" style={{backgroundColor:'#689BF0'}}>Register</Button>
+                </Link>
 
-          <Switch>
-            < Route path="/user">
-              <User />
-            </Route>
-            <Route path="/admin">
-              <Admin />
-            </Route>
-            {/* <Route path="/home">
-              <Home />
-            </Route> */}
-          </Switch>
-        </div>
-      </Router >
-    );
-  }
+            </div>
+        )
+    }
 }
-
-
-
-
-
-
-
-
 
 export default Home;
