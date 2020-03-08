3ae413ad4ea31fa4bc08aabc7235884a4a0a0032
Stew Esho <stewardesho@gmail.com>
2020-03-08 19:37:50 -0400

    added basic number stats

--- frontend/src/react-components/Dashboard/Admin/admin_dash.js
+++ frontend/src/react-components/Dashboard/Admin/admin_dash.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Grid from '@material-ui/core/Grid'
 import "./admin_styles.css";
 
 /* Primary Component for the Admin Dashboard page */
@@ -9,12 +10,29 @@ class AdminDashboard extends React.Component {
         <div id="navbar">
           navbar {/*TODO: replace with actual navbar component */}
         </div>
-        
+        <div className={"content"}>
+          <Grid container>
+            <NumberStat item title="Total Requests" stat={120} />
+            <NumberStat item title="Ready For Build" stat={60} />
+            <NumberStat item title="Ready For Paint" stat={12} />
+            <NumberStat item title="Ready For Delivery" stat={10} />
+          </Grid>
+        </div>
 
       </div>
     );
   }
 }
 
+function NumberStat(props){
+  return (
+    <div className={"number-stat"}>
+      <h2 className={"number-stat-title"}>{props.title}</h2>
+      <span className={"number-stat-num"}>
+        {props.stat}
+      </span>
+    </div>
+  );
+}
 
 export default AdminDashboard;
