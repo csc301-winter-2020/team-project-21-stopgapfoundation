9d3f1f9ad418fdf462a29208c168bfc6083e4a6d
Stew Esho <stewardesho@gmail.com>
2020-03-08 18:51:03 -0400

    set up files for the admin dashboard

--- frontend/src/react-components/Dashboard/Admin/admin_dash.js
+++ frontend/src/react-components/Dashboard/Admin/admin_dash.js
@@ -1,12 +1,15 @@
 import React from "react";
-import "./styles.css";
+import "./admin_styles.css";
 
 /* Primary Component for the Admin Dashboard page */
 class AdminDashboard extends React.Component {
   render () {
     return (
       <div>
-        <p> admin dashboard </p>
+        <div id="navbar">
+          navbar
+        </div>
+
       </div>
     );
   }
