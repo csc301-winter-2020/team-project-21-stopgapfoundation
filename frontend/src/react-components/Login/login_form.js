de0c57ef5e7e8d15bb5a1f02daaacb313cf4e3df
Stew Esho <stewardesho@gmail.com>
2020-03-08 17:51:33 -0400

    login and register forms now have html5 client-side form validation

--- frontend/src/react-components/Login/login_form.js
+++ frontend/src/react-components/Login/login_form.js
@@ -1,8 +1,5 @@
 import React from "react";
 
-import {
-  Link
-} from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
@@ -14,66 +11,80 @@ import Grid from '@material-ui/core/Grid';
  * @prop {function} goBack - use to return to parent login page.
  * @prop {boolean}  isAdmin - true iff the login form is for an admin
  */
-function LoginForm(props) {
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.form = React.createRef();
+  }
 
+  validate = () => {
+    return this.form.current.reportValidity();
+  }
   
-  return (
-    <div className="buttons center">
-      <form  >
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
-
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
-
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          onClick={(e) => {
-            e.preventDefault();
-            props.login({isAdmin: props.isAdmin})
-          }}
-        >
-            Sign in
-        </Button>
-
-        <Grid container>
-          <Grid item xs>
-            <Button className="center_link" onClick={props.goBack} >
-              Back to home page
-            </Button>
+  render() {
+    const props = this.props;
+    return (
+      <div className="buttons center">
+        <form ref={this.form} onSubmit={e => e.preventDefault()}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            type="email"
+            autoComplete="email"
+            autoFocus
+          />
+  
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+  
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            onClick={ e => {
+              e.preventDefault();
+              if (this.validate()){
+                console.log('uhh ok');
+                props.login({isAdmin: props.isAdmin})
+              }
+            }}
+          >
+              Sign in
+          </Button>
+  
+          <Grid container>
+            <Grid item xs>
+              <Button className="center_link" onClick={props.goBack} >
+                Back to home page
+              </Button>
+            </Grid>
+            { // We only allow clients to create new accounts. Stopgap accounts are made manually.
+            !props.isAdmin && 
+            <Grid item xs>
+              <Button className="center_link" onClick={props.register}>
+                Don't have an account?<br/>Sign Up!
+              </Button>
+            </Grid>}
+            
           </Grid>
-          { // We only allow clients to create new accounts. Stopgap accounts are made manually.
-          !props.isAdmin && 
-          <Grid item xs>
-            <Button className="center_link" onClick={props.register}>
-              Don't have an account?<br/>Sign Up!
-            </Button>
-          </Grid>}
-          
-        </Grid>
-      </form>
-    </div>
-  );
+        </form>
+      </div>
+    );
+  }
 }
 
 export default LoginForm;                                                                                                                                           
--- frontend/src/react-components/Login/register.js
+++ frontend/src/react-components/Login/register.js
@@ -4,71 +4,85 @@ import Button from "@material-ui/core/Button";
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 
-function RegisterForm(props) {
-  return (
-    <div className="buttons center">
-      <form>
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
+class RegisterForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.form = React.createRef();
+  }
 
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
+  validate = () => {
+    return this.form.current.reportValidity();
+  }
 
-
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          name="repeatpassword"
-          label="RepeatPassword"
-          type="repeatpassword"
-          id="repeatpassword"
-          autoComplete="current-password"
-        />
-
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          onClick={(e) => {
-            e.preventDefault();
-            props.login({isAdmin: false});
-          }} >
-          { /* TODO: feed in actual new user data */ }
-          Submit
-        </Button>
-
-        <Grid container>
-          <Grid item xs>
-            <Button fullWidth onClick={props.goBack}>
-              Go Back
-            </Button>
+  render() {
+    const props = this.props;
+    return (
+      <div className="buttons center">
+        <form ref={this.form} onSubmit={e => e.preventDefault()}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            type="email"
+            autoFocus
+          />
+  
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+  
+  
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="repeatpassword"
+            label="RepeatPassword"
+            type="password"
+            id="repeatpassword"
+            autoComplete="current-password"
+          />
+  
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            onClick={(e) => {
+              e.preventDefault();
+              if (this.validate())
+                props.login({isAdmin: props.isAdmin})
+            }} >
+            { /* TODO: feed in actual new user data */ }
+            Submit
+          </Button>
+  
+          <Grid container>
+            <Grid item xs>
+              <Button fullWidth onClick={props.goBack}>
+                Go Back
+              </Button>
+            </Grid>
           </Grid>
-        </Grid>
-
-      </form>
-
-    </div>
-  );
+  
+        </form>
+  
+      </div>
+    );
+  }
 }
 
 export default RegisterForm;  
