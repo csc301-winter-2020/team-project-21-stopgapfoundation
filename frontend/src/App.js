58562b80353be0995d59ce7deadec2dd363b7626
Stew Esho <stewardesho@gmail.com>
2020-03-08 15:07:08 -0400

    temporarily goes right to dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       user: {
-        loggedIn: false,
+        loggedIn: true,
         isAdmin: false
       }
     };
