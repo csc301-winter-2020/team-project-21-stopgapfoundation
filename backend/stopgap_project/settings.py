d3ab0acf7769af6817ab77bf4ebd2a8f11949556
samhrlee <sam.hr.lee@gmail.com>
2020-03-09 13:09:27 -0400

    Add default permissions for Django REST

--- backend/stopgap_project/settings.py
+++ backend/stopgap_project/settings.py
@@ -123,6 +123,9 @@ STATIC_URL = '/static/'
 
 # Django REST framework settings
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
     # Control how many objects are returned per page
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
