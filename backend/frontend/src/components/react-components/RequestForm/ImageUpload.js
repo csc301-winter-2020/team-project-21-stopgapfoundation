b64a8e4757ad039b8628aa94eb1f8f11226a23d5
Sam Lee <sam.hr.lee@gmail.com>
2020-03-11 13:17:58 -0400

    Add ImageUpload, LiabilityWaiver as cases for Ramp request form

---
+++ backend/frontend/src/components/react-components/RequestForm/ImageUpload.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import FormUserDetails from './FormUserDetails';
+
+export class ImageUpload extends Component {
+    
+}
+
+export default ImageUpload;
--- backend/frontend/src/components/react-components/RequestForm/index.js
+++ backend/frontend/src/components/react-components/RequestForm/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import FormUserDetails from './FormUserDetails';
+import ImageUpload from './ImageUpload';
+import LiabilityWaiver from './LiabilityWaiver';
 
 export class UserForm extends Component {
     state = {
@@ -48,11 +50,21 @@ export class UserForm extends Component {
                     />
                 )
             case 2:
-                return <h1> Thank you! We'll be in contact about your request. </h1>
+                return (
+                    <ImageUpload
+                        nextStep={this.nextStep}
+                    />
+                )
+
             case 3:
-                return <h1>Confirm</h1>
+                return (
+                    <LiabilityWaiver
+                        nextStep={this.nextStep}
+                        values = {values}
+                    />
+                )
             case 4:
-                return <h1>Success</h1>
+                return <h1> Thank you! We'll be in contact about your request. </h1>
         }
         return (
             <div>
