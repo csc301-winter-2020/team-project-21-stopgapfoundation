13bfefc9253e7269cf63788d9f59cf9885d429bf
Stew Esho <stewardesho@gmail.com>
2020-03-11 20:26:30 -0400

    updated status block so it uses a dropdown menu; added animations to progressBar component

--- frontend/src/react-components/Dashboard/Admin/StatusBlock.js
+++ frontend/src/react-components/Dashboard/Admin/StatusBlock.js
@@ -2,20 +2,27 @@ import React from "react";
 import ProgressBar from "../ProgressBar";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-import TextField from "@material-ui/core/TextField";
+import {Select, MenuItem, InputLabel} from "@material-ui/core";
 import { sizing } from '@material-ui/system';
 
 class StatusBlock extends React.Component {
 
   constructor(props){
     super(props);
+    this.statuses = [
+      "Request Recieved",
+      "Request Accepted",
+      "Build Phase",
+      "Paint Phase",
+      "Out for Delivery"
+    ];
+
     this.state = {
-      progressInput: "",
-      statusInput: "",
-      progress: 20,
-      status: "Build Phase"
+      statusInput: this.statuses[0],
+      progress: (0 / (this.statuses.length - 1)) * 100,
+      status: this.statuses[0]
     };
-  }
+  }  
 
   handleStatusInput = e => {
     this.setState({
@@ -23,16 +30,10 @@ class StatusBlock extends React.Component {
     });
   }
 
-  handleProgressInput = e => {
-    this.setState({
-      progressInput: e.target.value
-    });
-  }
-
   saveData = e => {
     e.preventDefault();
-    const progress = parseInt(this.state.progressInput);
     const status = this.state.statusInput;
+    const progress = (this.statuses.indexOf(status) / (this.statuses.length - 1)) * 100
     this.setState({
       progress,
       status
@@ -41,48 +42,36 @@ class StatusBlock extends React.Component {
 
   render() {
 
-    const dirtyBit = this.state.progressInput.length > 0 || this.state.statusInput.length > 0;
+    const dirtyBit = this.state.statusInput != this.state.status;
 
     return (
       <div className={"block"}>
         <h2 className={"block-title"}>
           Status
         </h2>
+        <span className={"status-block-last-updated"}>
+          Last Updated: <strong>
+            {"March 11" /*TODO: get actual date */}
+          </strong>
+        </span>
         <ProgressBar progress={this.state.progress / 100} />
-        <span className={"status-block-status"} style={{left: `${this.state.progress}%`}}>
+        <span className={"status-block-status"} > { /* style={{left: `${this.state.progress}%`}} */}
           {this.state.status}
+          <br/>
         </span>
         { this.props.isAdmin &&
           <Grid container>
-            <Grid item md={5}>
-              <TextField
-                className = {"status-bar-textfield"}
-                value={this.state.progressInput}
-                onChange={this.handleProgressInput}
-                placeholder={this.state.progress.toString()}
-                label="Progress"
-                type="number"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                variant="outlined"
-              />
-              %
-            </Grid>
-
-            <Grid item md={5}>
-              <TextField
+            <Grid item md={10}>
+              <Select
                 className = {"status-bar-textfield"}
                 value={this.state.statusInput}
                 onChange={this.handleStatusInput}
-                placeholder={this.state.status}
-                label="Status"
-                type="text"
-                InputLabelProps={{
-                  shrink: true,
-                }}
+                type="number"
                 variant="outlined"
-              />
+              >
+                {this.statuses.map((status, i) => <MenuItem value={status} key={i}>{status}</MenuItem>)}
+                
+              </Select>
             </Grid>
 
             <Grid item md={2}>
