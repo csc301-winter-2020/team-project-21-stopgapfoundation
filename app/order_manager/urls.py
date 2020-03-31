f4f529b0a4b2cdf273e5fc25c797620e6b1488a7
Stew Esho <stewardesho@gmail.com>
2020-03-31 14:45:44 -0400

    Added JWT Token support, which allows frontend to request authentication tokens for valid users in the backend

--- app/order_manager/urls.py
+++ app/order_manager/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from order_manager import views
+from rest_framework_simplejwt import views as jwt_views
 
 
 router = routers.DefaultRouter()
@@ -9,5 +10,7 @@ router.register(r'order-information', views.OrderViewSet)
 router.register(r'waiver-information', views.WaiverViewSet)
 
 urlpatterns =[
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
