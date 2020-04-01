9e09e9d7b5b4165f7ec71e59c2fe9426296eedc0
samhrlee <sam.hr.lee@gmail.com>
2020-03-31 22:04:41 -0400

    Remove reference to Client model

--- app/order_manager/urls.py
+++ app/order_manager/urls.py
@@ -4,7 +4,6 @@ from order_manager import views
 
 
 router = routers.DefaultRouter()
-router.register(r'client-information', views.ClientViewSet)
 router.register(r'order-information', views.OrderViewSet)
 router.register(r'waiver-information', views.WaiverViewSet)
 
