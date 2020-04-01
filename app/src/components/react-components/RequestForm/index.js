f815c49322c4ea410e6fa762d909f4a6a2518957
Andrey Lapin <lapinandrey95@gmail.com>
2020-04-01 12:58:15 -0400

    Fixed indentation

--- app/src/components/react-components/RequestForm/index.js
+++ app/src/components/react-components/RequestForm/index.js
@@ -6,12 +6,12 @@ import { Redirect, Link } from 'react-router-dom';
 
 export class UserForm extends Component {
     state = {
-        step: 1,
+        step: 1, 
         firstName: '',
         lastName: '',
         email: '',
         companyName: '',
-        phoneNumber: '',
+        phoneNumber: '', 
         deliveryAddress: '',
         deliveryType: '',
         rampColor: '',
@@ -29,154 +29,152 @@ export class UserForm extends Component {
     }
 
     prevStep = () => {
-            const { step } = this.state;
-            this.setState({
-                step: step - 1
-            });
-        }
-        // Handle Field Changes
-    handleChange = input => e => {
+        const { step } = this.state;
         this.setState({
-            [input]: e.target.value })
+            step: step - 1
+        });
     }
-
+    // Handle Field Changes
+    handleChange = input => e => {
+        this.setState({[input]: e.target.value})
+    }
+	
     handleSubmitOrder = () => {
-        const data = {
-            "user": 1,
-            "billing_address": this.state.deliveryAddress,
-            "shipping_address": this.state.billingAddress,
-            "waiver": 1,
-            // "entryway_photo":"/order-information/data/user_2/step_photos/200XY_hnIn8xv.png",
-            // "step_left_photo":"/order-information/data/user_2/step_photos/15000.png",
-            // "step_right_photo":"/order-information/data/user_2/step_photos/gan1.png",
-            "step_left_height": this.state.leftStepHeight,
-            "step_right_height": this.state.rightStepHeight,
-            "ramp_colour": this.state.ramp_colour,
-            "delivery_method": this.state.deliveryType,
-            "subsidize": false,
-            "status": "Request Recieved",
-            "notes": "",
-            "firstName": this.state.firstName,
-            "lastName": this.state.lastName,
-            "email": this.state.email,
-            "companyName": this.state.companyName,
-            "phoneNumber": this.state.phoneNumber
-        }
-
-        const token = localStorage.getItem('token-access')
+        const data ={
+		"user":1,
+        "billing_address":this.state.deliveryAddress,
+        "shipping_address":this.state.billingAddress,
+        "waiver":1,
+        // "entryway_photo":"/order-information/data/user_2/step_photos/200XY_hnIn8xv.png",
+        // "step_left_photo":"/order-information/data/user_2/step_photos/15000.png",
+        // "step_right_photo":"/order-information/data/user_2/step_photos/gan1.png",
+        "step_left_height":this.state.leftStepHeight,
+        "step_right_height":this.state.rightStepHeight,
+        "ramp_colour":this.state.ramp_colour,
+        "delivery_method":this.state.deliveryType,
+        "subsidize":false,
+        "status":"Request Recieved",
+		"notes":"",
+        "firstName":this.state.firstName,
+        "lastName":this.state.lastName,
+        "email":this.state.email,
+        "companyName":this.state.companyName,
+        "phoneNumber":this.state.phoneNumber}
+		
+		const token = localStorage.getItem('token-access')
         const requestOptions = {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token
-            },
+				'Content-Type': 'application/json',
+				'Authorization': 'Bearer ' + token
+			},
             body: JSON.stringify(data)
         };
         fetch('/order-information/', requestOptions)
             .then(async response => {
                 const data = await response.json();
-
+    
                 // check for error response
                 if (!response.ok) {
                     // get error message from body or default to response status
                     const error = (data && data.message) || response.status;
                     return Promise.reject(error);
                 }
-                console.log(data.pk)
+				console.log(data.pk)
                 this.setState({ postId: data.pk })
             })
             .catch(error => {
                 this.setState({ errorMessage: error });
                 console.error('There was an error!', error);
-            });
+            }); 
     }
-
-    handleSubmitWaiver = () => {
-        const data = {
-            //"pk":1,
-            "user": 1,
-            "signatory_first_name": this.state.managerFirst,
-            "signatory_last_name": this.state.managerFirst,
-            "signatory_signature": this.state.witnessSig,
-            "witness_first_name": this.state.witnessName,
-            "witness_last_name": this.state.witnessName,
-            "witness_signature": this.state.witnessSig
-        }
-        const token = localStorage.getItem('token-access')
+	
+	handleSubmitWaiver = () => {
+		const data ={
+		//"pk":1,
+		"user":1,
+        "signatory_first_name":this.state.managerFirst,
+        "signatory_last_name":this.state.managerFirst,
+        "signatory_signature":this.state.witnessSig,
+        "witness_first_name":this.state.witnessName,
+        "witness_last_name":this.state.witnessName,
+        "witness_signature":this.state.witnessSig}
+		const token = localStorage.getItem('token-access')
         const requestOptions = {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token
-            },
+				'Content-Type': 'application/json',
+				'Authorization': 'Bearer ' + token
+			},
             body: JSON.stringify(data)
         };
         fetch('/waiver-information/', requestOptions)
             .then(async response => {
                 const data = await response.json();
-
+    
                 // check for error response
                 if (!response.ok) {
                     // get error message from body or default to response status
                     const error = (data && data.message) || response.status;
                     return Promise.reject(error);
                 }
-                console.log(data.pk)
+				console.log(data.pk)
                 this.setState({ postId: data.pk })
             })
             .catch(error => {
                 this.setState({ errorMessage: error });
                 console.error('There was an error!', error);
-            });
-    }
+            }); 
+	}
 
     render() {
         const { step } = this.state;
-        const { firstName, lastName, email, companyName, phoneNumber, billingAddress, deliveryAddress, deliveryType, rampColor, rightStepHeight, leftStepHeight } = this.state;
-
+        const { firstName, lastName, email, companyName, phoneNumber,billingAddress, deliveryAddress, deliveryType, rampColor,rightStepHeight, leftStepHeight } = this.state;
+   
 
         switch (step) {
             case 0:
-                return <Redirect to = "/dashboard" / >
-                    case 1:
-                return ( <
-                    FormUserDetails nextStep = { this.nextStep }
-                    handleChange = { this.handleChange }
-
+                return <Redirect to="/dashboard" />
+            case 1: 
+                return (
+                    <FormUserDetails
+                        nextStep={this.nextStep}
+                        handleChange= {this.handleChange}
+                    
                     />
                 )
             case 2:
-                return ( <
-                    ImageUpload prevStep = { this.prevStep }
-                    nextStep = { this.nextStep }
-                    handleChange = { this.handleChange }
+                return (
+                    <ImageUpload
+						prevStep={this.prevStep}
+                        nextStep={this.nextStep}
+                        handleChange= {this.handleChange}
                     />
                 )
             case 3:
-                return ( <
-                    LiabilityWaiver nextStep = { this.nextStep }
-                    prevStep = { this.prevStep }
-                    handleSubmitWaiver = { this.handleSubmitWaiver }
-                    handleSubmitOrder = { this.handleSubmitOrder }
-                    handleChange = { this.handleChange }
+                return (
+                    <LiabilityWaiver
+                        nextStep={this.nextStep}
+                        prevStep={this.prevStep}
+						handleSubmitWaiver={this.handleSubmitWaiver}
+						handleSubmitOrder={this.handleSubmitOrder}
+                        handleChange={this.handleChange}
                     />
                 )
-            case 4:
-                return ( <
-                    div >
-                    <
-                    h1 > Thank you!We 'll be in contact about your request.</h1> <
-                    Link to = "/dashboard" >
-                    Go back home. <
-                    /Link> <
-                    /div>
+            case 4: 
+                return (
+                    <div>
+                        <h1> Thank you! We'll be in contact about your request.</h1>
+                        <Link to="/dashboard">
+                            Go back home.
+                        </Link>
+                    </div>
                 )
         }
-        return ( <
-            div >
-
-            <
-            /div>
+        return (
+            <div>
+                
+            </div>
         )
     }
 }
