3635879628bfc440926c56031c676dcc4ceb4d23
samhrlee <sam.hr.lee@gmail.com>
2020-03-31 21:59:43 -0400

    Remove client model

--- app/order_manager/admin.py
+++ app/order_manager/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Client, Order, Waiver
+from .models import Order, Waiver
 
-admin.site.register(Client)
 admin.site.register(Order)
 admin.site.register(Waiver)
