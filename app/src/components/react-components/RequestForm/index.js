ea66a18e7a68852b6695bcd8869806f8fc217262
josh_zhang <josh.zhang@mail.utoronto.ca>
2020-04-10 15:58:37 -0400

    Fix can not save order

--- app/src/components/react-components/RequestForm/index.js
+++ app/src/components/react-components/RequestForm/index.js
@@ -74,7 +74,7 @@ export class UserForm extends Component {
 	
     handleSubmitOrder = () => {
         const data ={
-        "pk":1,
+        // "pk":1,
         "user":1,
         "first_name":this.state.firstName,
         "last_name":this.state.lastName,
@@ -87,8 +87,10 @@ export class UserForm extends Component {
         "entryway_photo": this.state.entrywayPhoto,
         "step_left_photo": this.state.stepLeftPhoto,
         "step_right_photo": this.state.stepRightPhoto,
-        "step_left_height":parseFloat(this.state.leftStepHeight),
-        "step_right_height":parseFloat(this.state.rightStepHeight),
+        // "step_left_height":parseFloat(this.state.leftStepHeight),
+        // "step_right_height":parseFloat(this.state.rightStepHeight),
+        "step_left_height":this.state.leftStepHeight,
+        "step_right_height":this.state.rightStepHeight,
         "ramp_colour":this.state.rampColor,
         "delivery_method":this.state.deliveryType,
         "subsidize":this.state.subsidize,
@@ -97,15 +99,19 @@ export class UserForm extends Component {
         }
 
         console.log(data)
+        console.log(JSON.stringify(data))
 		
 		const token = localStorage.getItem('token-access')
         const requestOptions = {
             method: 'POST',
             headers: {
-				'Content-Type': 'application/json',
-				'Authorization': 'Bearer ' + token
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + token
+               
 			},
             body: JSON.stringify(data)
+         
         };
         fetch('/order-information/', requestOptions)
             .then(async response => {
@@ -130,42 +136,42 @@ export class UserForm extends Component {
     }
 	
 	handleSubmitWaiver = () => {
-		const data ={
-		//"pk":1,
-		"user":1,
-        "signatory_first_name":this.state.managerFirst,
-        "signatory_last_name":this.state.managerFirst,
-        "signatory_signature":this.state.witnessSig,
-        "witness_first_name":this.state.witnessName,
-        "witness_last_name":this.state.witnessName,
-        "witness_signature":this.state.witnessSig}
+		// const data ={
+		// //"pk":1,
+		// "user":1,
+        // "signatory_first_name":this.state.managerFirst,
+        // "signatory_last_name":this.state.managerFirst,
+        // "signatory_signature":this.state.witnessSig,
+        // "witness_first_name":this.state.witnessName,
+        // "witness_last_name":this.state.witnessName,
+        // "witness_signature":this.state.witnessSig}
 
-		const token = localStorage.getItem('token-access')
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-				'Content-Type': 'application/json',
-				'Authorization': 'Bearer ' + token
-			},
-            body: JSON.stringify(data)
-        };
-        fetch('/waiver-information/', requestOptions)
-            .then(async response => {
-                const data = await response.json();
+		// const token = localStorage.getItem('token-access')
+        // const requestOptions = {
+        //     method: 'POST',
+        //     headers: {
+		// 		'Content-Type': 'application/json',
+		// 		'Authorization': 'Bearer ' + token
+		// 	},
+        //     body: JSON.stringify(data)
+        // };
+        // fetch('/waiver-information/', requestOptions)
+        //     .then(async response => {
+        //         const data = await response.json();
     
-                // check for error response
-                if (!response.ok) {
-                    // get error message from body or default to response status
-                    const error = (data && data.message) || response.status;
-                    return Promise.reject(error);
-                }
-				console.log(data)
-                this.setState({ postId: data.pk })
-            })
-            .catch(error => {
-                this.setState({ errorMessage: error });
-                console.error('There was an error!', error);
-            }); 
+        //         // check for error response
+        //         if (!response.ok) {
+        //             // get error message from body or default to response status
+        //             const error = (data && data.message) || response.status;
+        //             return Promise.reject(error);
+        //         }
+		// 		console.log(data)
+        //         this.setState({ postId: data.pk })
+        //     })
+        //     .catch(error => {
+        //         this.setState({ errorMessage: error });
+        //         console.error('There was an error!', error);
+        //     }); 
 	}
 
     render() {
