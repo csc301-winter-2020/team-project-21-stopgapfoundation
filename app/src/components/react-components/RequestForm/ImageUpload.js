2b8d97c08c3f0702e6c215763b8ab877c2c666d0
Daven <davenrai@gmail.com>
2020-04-10 20:24:53 -0400

    fixed styling for imageupload page

--- app/src/components/react-components/RequestForm/ImageUpload.js
+++ app/src/components/react-components/RequestForm/ImageUpload.js
@@ -30,8 +30,83 @@ export class ImageUpload extends Component {
               Ramp Request Form
                         </Typography>
           </AppBar>
-          <br />
 
+
+          <div style={{ marginLeft: '7vh', marginTop: '5vh', height: '100%' }}>
+            <h1>Upload Ramp Images:</h1>
+            <h3>StopGap requires three images to process your ramp request.</h3>
+
+
+
+            <form onSubmit={this.continue}>
+              <ul>
+                <li>
+                  <h4>Photo of entire entryway</h4>
+                  <div style={{ maxWidth: '50vh' }}>
+                    Take a look at the entryway and envision the placement of the ramp.
+                    Take a picture of the entire entrance including the step in front of the doorway and the sidewalk.
+                    For a safe turning radius, you will need 42″ of space in addition to the length of the ramp (which is 6 times the height of the step).
+                </div>
+                <br />
+                <div><Upload imageIdentifier="entrywayPhoto" handlePicture={this.props.handlePicture} required></Upload></div>
+                </li>
+                <li>
+                  <h4>Photo of Left Side Step</h4>
+                  <div><Upload imageIdentifier="stepLeftPhoto" handlePicture={this.props.handlePicture} required></Upload></div>
+                </li>
+                <li>
+                  <h4>Photo of Right Side Step</h4>
+                  <div><Upload imageIdentifier="stepRightPhoto" handlePicture={this.props.handlePicture} required></Upload></div>
+                </li>
+              </ul>
+            </form>
+            <br />
+            <hr style={{maxWidth:'90%'}}></hr>
+            <br />
+            <div style={{ maxWidth: '50vh' }}>
+              Please estimate the height of your step and enter them below.
+              Your measurement selection will be verified by StopGap staff by evaluating your measurement photo submissions.
+              We may request new or additional photos to best design the ramp to fit your step.
+                        </div>
+            <br />
+            <TextField
+              required
+              style={{ width: '30vh' }}
+              type="number"
+              inputProps={{ min: "0", max: "100", step: "0.01" }}
+              id="standard-required"
+              label="Step Height - Left"
+              defaultValue=""
+              onChange={this.props.handleChange('leftStepHeight')}
+            />
+            <br /> <br />
+            <TextField
+              required
+              style={{ width: '30vh' }}
+              type="number"
+              inputProps={{ min: "0", max: "100", step: "0.01" }}
+              id="standard-required"
+              label="Step Height - Right"
+              defaultValue=""
+              onChange={this.props.handleChange('rightStepHeight')}
+            />
+            <br /> <br />
+            <div style={{ marginBottom: '10px' }}>
+              <Button
+                style={{ right: 10 }} color="primary" variant="contained" onClick={this.back}>Previous
+              </Button>
+              <Button
+                type="submit" color="primary" variant="contained" onClick={this.continue}>Next
+              </Button>
+            </div>
+
+
+          </div>
+
+
+
+
+          {/* 
           <div id="form" style={{ marginLeft: '7vh', marginTop: '5vh', height:'70vh'}}>
             <h1>Upload Ramp Images:</h1>
             <h3>StopGap requires three images to process your ramp request.</h3>
@@ -100,7 +175,7 @@ export class ImageUpload extends Component {
               </Button>
               <br/>
               </div>
-          </div>
+          </div> */}
           {/* <form onSubmit={this.continue} style={{ marginLeft: '5vh', marginTop: '5vh', marginRight: '20vh' }}>
             <h1>Upload your Images:</h1>
             <h3>StopGap requires three images to process your ramp request:</h3>
